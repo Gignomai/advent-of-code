656d04cb6bf8c20db91c0815d2788bdeea5eefb1
Manel Márquez <gignomaicsl@google.com>
2023-12-03 09:24:22 +0100

    Day 2 WIP: some refactor.

--- 2023/day2/src/Game.java
+++ 2023/day2/src/Game.java
@@ -1,2 +1,32 @@
 public class Game {
+    private int id;
+    private boolean isValid;
+
+    public Game(String game) {
+        isValid = true;
+        id = Integer.parseInt(game.split(":")[0].split(" ")[1]);
+        String plays = game.split(":")[1];
+
+        for (String play : plays.split(";")) {
+            String[] draws = play.split(",");
+            for (String draw: draws) {
+                String[] parts = draw.trim().split(" ");
+                int number = Integer.parseInt(parts[0]);
+                String color = parts[1];
+                if ((color.equals("red") && number > 12)
+                        || (color.equals("green") && number > 13)
+                        || (color.equals("blue") && number > 14)) {
+                    isValid = false;
+                }
+            }
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
 }
--- 2023/day2/src/Part1.java
+++ 2023/day2/src/Part1.java
@@ -3,33 +3,11 @@ import java.util.List;
 public class Part1 {
 
     public Integer processLines(List<String> lines) {
-        int total = 0;
-
-        for (String game : lines) {
-            boolean isValid = true;
-            int id = Integer.parseInt(game.split(":")[0].split(" ")[1]);
-            String plays = game.split(":")[1];
-
-            for (String play : plays.split(";")) {
-                String[] draws = play.split(",");
-                for (String draw: draws) {
-                    String[] parts = draw.trim().split(" ");
-                    int number = Integer.parseInt(parts[0]);
-                    String color = parts[1];
-                    if ((color.equals("red") && number > 12)
-                            || (color.equals("green") && number > 13)
-                            || (color.equals("blue") && number > 14)) {
-                        isValid = false;
-                    }
-                }
-            }
-
-            if (isValid) {
-                total += id;
-            }
-        }
-
-        return total;
+        return lines.stream()
+                .map(Game::new)
+                .filter(Game::isValid)
+                .mapToInt(Game::getId)
+                .sum();
     }
 
     public boolean test(List<String> lines) {
