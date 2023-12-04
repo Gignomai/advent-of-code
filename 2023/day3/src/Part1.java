d6a2f9b97880415da082c6078ed5065482478a8b
Manel Márquez <gignomaicsl@google.com>
2023-12-04 12:12:07 +0100

    Day 3 WIP: some refactor.

--- 2023/day3/src/Part1.java
+++ 2023/day3/src/Part1.java
@@ -1,27 +1,105 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Part1 {
 
+    public Integer processLines(List<String> lines) {
+        Set<Integer> parts = new HashSet<>();
+        List<Map<Integer, String>> numbers = new ArrayList<>();
+        List<List<Integer>> symbols = new ArrayList<>();
 
-    public Long processLines(List<String> lines) {
-        return lines.stream()
-                .mapToLong(Part1::getPriority)
+        for (int i = 0; i < lines.size(); i++) {
+            Map<Integer, String> nums = getNumbersFromLine(lines.get(i));
+            List<Integer> syms = getSymbolsFromLine(lines.get(i));
+
+            for (Entry<Integer, String> entry: nums.entrySet()) {
+                String[] range = entry.getValue().split(",");
+                int start = Integer.parseInt(range[0]);
+                int end = Integer.parseInt(range[1]);
+
+                for (Integer symPos: syms) {
+                    if (symPos == start - 1 || symPos == end + 1) {
+                        parts.add(entry.getKey());
+                        break;
+                    }
+                }
+            }
+
+            if (i > 0) {
+                for (Entry<Integer, String> entry: nums.entrySet()) {
+                    String[] range = entry.getValue().split(",");
+                    int start = Integer.parseInt(range[0]);
+                    int end = Integer.parseInt(range[1]);
+
+                    for (Integer symPos: symbols.get(i - 1)) {
+                        if (symPos >= start - 1 && symPos <= end + 1) {
+                            parts.add(entry.getKey());
+                            break;
+                        }
+                    }
+                }
+
+                for (Integer symPos: syms) {
+                    for (Entry<Integer, String> entry: numbers.get(i - 1).entrySet()) {
+                        String[] range = entry.getValue().split(",");
+                        int start = Integer.parseInt(range[0]);
+                        int end = Integer.parseInt(range[1]);
+
+                        if (symPos >= start - 1 && symPos <= end + 1) {
+                            parts.add(entry.getKey());
+                            break;
+                        }
+                    }
+                }
+            }
+
+            numbers.add(nums);
+            symbols.add(syms);
+        }
+
+        System.out.println("symbols = " + symbols);
+        System.out.println("numbers = " + numbers);
+
+        return parts.stream()
+                .mapToInt(Integer::intValue)
                 .sum();
     }
 
-    private static Integer getPriority(String s) {
-        String part1 = s.substring(0, s.length() / 2);
-        String part2 = s.substring(s.length() / 2);
+    private List<Integer> getSymbolsFromLine(String line) {
+        List<Integer> result = new ArrayList<>();
+        Pattern pattern = Pattern.compile("([^.0-9])");
+        Matcher matcher = pattern.matcher(line);
 
-        return part1.chars()
-                .filter(c -> part2.indexOf(c) >= 0)
-                .mapToObj(c -> (char) c)
-                .findAny()
-                .map(finding -> Day3.LETTERS.indexOf(finding) + 1)
-                .orElseThrow();
+        while (matcher.find()) {
+            result.add(matcher.start());
+        }
+
+        return result;
+    }
+
+    private Map<Integer, String> getNumbersFromLine(String line) {
+        HashMap<Integer, String> result = new HashMap<>();
+        Pattern pattern = Pattern.compile("(\\d+)");
+        Matcher matcher = pattern.matcher(line);
+
+        while (matcher.find()) {
+            String group = matcher.group(1);
+            int number = Integer.parseInt(group);
+            String range = matcher.start() + "," + (matcher.end() - 1);
+            result.put(number, range);
+        }
+
+        return result;
     }
 
     public boolean test(List<String> lines) {
-        return processLines(lines).equals(157L);
+        return processLines(lines).equals(4361);
     }
 }
--- 2023/day3/src/Part2.java
+++ 2023/day3/src/Part2.java
@@ -1,46 +1,11 @@
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class Part2 {
 
     public Long processLines(List<String> lines) {
-        return getGroups(lines).stream()
-                .mapToLong(this::getPriority)
-                .sum();
+        return 0L;
 
-    }
-
-    private List<List<String>> getGroups(List<String> rucksacks) {
-        List<List<String>> groups = new ArrayList<>();
-        List<String> group = new ArrayList<>();
-        for (int i = 0; i < rucksacks.size(); i++) {
-            group.add(rucksacks.get(i));
-            if ((i + 1) % 3 == 0) {
-                groups.add(group);
-                group = new ArrayList<>();
-            }
-        }
-
-        return groups;
-    }
-
-    private Integer getPriority(List<String> group) {
-
-        char finding = ' ';
-
-        String r1 = group.get(0);
-        String r2 = group.get(1);
-        String r3 = group.get(2);
-
-        for (int i = 0; i < r1.length(); i++) {
-            if (r2.indexOf(r1.charAt(i)) >= 0 && r3.indexOf(r1.charAt(i)) >= 0) {
-                finding = r1.charAt(i);
-                break;
-            }
-        }
-
-        return Day3.LETTERS.indexOf(finding) + 1;
     }
 
     public boolean test(List<String> fileName) {
